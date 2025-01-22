a2c1ac58c90fbde2b18cab6b1e227513c1d95a9b
Mohamed-Gamal00 <mohamedgamal1907a@gmail.com>
2025-01-22 17:08:31 +0200

    delete currentAccessToken in logut

--- app/Http/Controllers/Api/UserAuthController.php
+++ app/Http/Controllers/Api/UserAuthController.php
@@ -18,136 +18,134 @@ use Illuminate\Support\Facades\Validator;
 
 class UserAuthController extends Controller
 {
-    protected $sms_service;
-    protected $moraSms;
-    protected $userService;
-    protected $telephoneVerification;
+  protected $sms_service;
+  protected $moraSms;
+  protected $userService;
+  protected $telephoneVerification;
 
-    public function __construct(VerificationServices $services, moraSms $moraSmsGateway,TelephoneVerification $telephoneVerification , UserService $userService)
-    {
-        $this->sms_service = $services;
-        $this->moraSms = $moraSmsGateway;
-        $this->userService = $userService;
-        $this->telephoneVerification = $telephoneVerification;
+  public function __construct(VerificationServices $services, moraSms $moraSmsGateway, TelephoneVerification $telephoneVerification, UserService $userService)
+  {
+    $this->sms_service = $services;
+    $this->moraSms = $moraSmsGateway;
+    $this->userService = $userService;
+    $this->telephoneVerification = $telephoneVerification;
+  }
+
+  public function register(RegisterRequest $request)
+  {
+    $user = null;
+
+    $user = $this->userService->createUser($request);
+
+    $data = [
+      'verification_required' => true,
+      'user_id' => $user->id,
+    ];
+    return $this->telephoneVerification->SendVerificationCode($user, $data);
+  }
+
+  public function verifyCode(Request $request)
+  {
+    $verificationData = $request->validate([
+      'user_id' => 'required',
+      'code' => 'required',
+    ]);
+
+    $user = User::find($verificationData['user_id']);
+
+    if (!$user) {
+      return ApiResponse::sendResponse(404, "User not found");
+    }
+    /*check expired code*/
+    if ($this->userService->isVerificationCodeExpired($user)) {
+      return ApiResponse::sendResponse(403, "انتهت صلاحية رمز التحقق. يرجى طلب واحد جديدة.");
     }
 
-    public function register(RegisterRequest $request)
-    {
-        $user = null;
+    $isValidCode = $this->sms_service->checkOTPCodePassword($user->id, $verificationData['code']);
 
-        $user = $this->userService->createUser($request);
+    if ($isValidCode) {
+      $this->userService->markUserVerified($user, $request->code);
+      $token = $user->createToken('-AuthToken')->plainTextToken;
+      $data = [
+        'token' => $token,
+      ];
 
-        $data = [
-            'verification_required' => true,
-            'user_id' => $user->id,
-        ];
-        return $this->telephoneVerification->SendVerificationCode($user, $data);
+      return ApiResponse::sendResponse(200, 'Verification successful', $data);
+    } else {
+      return ApiResponse::sendResponse(403, 'Invalid verification code');
+    }
+  }
+
+  public function login(LoginRequest $request)
+  {
+    $user = User::where('phone_number', $request->phone_number)->first();
+    if ($user && Hash::check($request->password, $user->password)) {
+      $token = $user->createToken('-AuthToken')->plainTextToken;
+      $data = [
+        'token' => $token,
+      ];
+
+      return ApiResponse::sendResponse(200, 'Login Successfully', $data);
+    }
+    return ApiResponse::sendResponse(401, 'Login Failed', []);
+  }
+
+
+  public function logout()
+  {
+    auth()->user()->currentAccessToken()->delete();
+    return ApiResponse::sendResponse(200, 'logged out');
+  }
+
+  public function forgetPassword(Request $request)
+  {
+    $user = User::where('phone_number', $request->phone_number)->first();
+    if (!$user) {
+      return ApiResponse::sendResponse(200, 'this phone_number not exist');
+    }
+    $data = [
+      'verification_required' => true,
+      'user_id' => $user->id,
+    ];
+    return $this->telephoneVerification->SendVerificationCode($user, $data);
+  }
+
+
+  public function resetPassword(Request $request)
+  {
+    $user = User::where('id', $request->user_id)->first();
+
+    if ($this->userService->isVerificationCodeExpired($user)) {
+      return ApiResponse::sendResponse(403, "انتهت صلاحية رمز التحقق. يرجى طلب واحد جديدة.");
     }
 
-    public function verifyCode(Request $request)
-    {
-        $verificationData = $request->validate([
-            'user_id' => 'required',
-            'code' => 'required',
-        ]);
+    $isValidCode = $this->sms_service->checkOTPCodePassword($user->id, $request->code_verify);
+    if ($isValidCode) {
+      $request->validate(['password' => 'required|string|min:6|confirmed']);
+      $user->update(['password' => $request->password]);
+      return ApiResponse::sendResponse(200, 'Password reset successfully.');
+    } else {
+      return ApiResponse::sendResponse(200, 'Invalid verification code');
+    }
+  }
 
-        $user = User::find($verificationData['user_id']);
+  public function resendVerifyCode(Request $request)
+  {
+    $validator = Validator::make($request->all(), [
+      'user_id' => 'required',
+    ]);
 
-        if (!$user) {
-            return response()->json(['message' => 'User not found'], 404);
-        }
-        /*check expired code*/
-        if ($this->userService->isVerificationCodeExpired($user)) {
-            return ApiResponse::sendResponse(403, "انتهت صلاحية رمز التحقق. يرجى طلب واحد جديدة.");
-        }
-
-        $isValidCode = $this->sms_service->checkOTPCodePassword($user->id, $verificationData['code']);
-
-        if ($isValidCode) {
-            $this->userService->markUserVerified($user, $request->code);
-            $token = $user->createToken('-AuthToken')->plainTextToken;
-            $data = [
-                'token' => $token,
-            ];
-
-            return ApiResponse::sendResponse(200, 'Verification successful', $data);
-        } else {
-            return ApiResponse::sendResponse(403, 'Invalid verification code');
-        }
+    if ($validator->fails()) {
+      return ApiResponse::sendResponse(422, "Validation Error", $validator->errors());
     }
 
-    public function login(LoginRequest $request)
-    {
-        $user = User::where('phone_number', $request->phone_number)->first();
-        if ($user && Hash::check($request->password, $user->password)) {
-            $token = $user->createToken('-AuthToken')->plainTextToken;
-            $data = [
-                'token' => $token,
-            ];
-
-            return ApiResponse::sendResponse(200, 'Login Successfully', $data);
-        }
-        return ApiResponse::sendResponse(401, 'Login Failed', []);
+    $user_id = $request->user_id;
+    $user = User::find($user_id);
+    if (!$user) {
+      return 'user id not found';
     }
 
-
-    public function logout()
-    {
-        auth()->user()->tokens()->delete();
-        return ApiResponse::sendResponse(200, 'logged out');
-    }
-
-    public function forgetPassword(Request $request)
-    {
-        $user = User::where('phone_number', $request->phone_number)->first();
-        if (!$user) {
-            return ApiResponse::sendResponse(200, 'this phone_number not exist');
-        }
-        $data = [
-            'verification_required' => true,
-            'user_id' => $user->id,
-        ];
-        return $this->telephoneVerification->SendVerificationCode($user, $data);
-    }
-
-
-    public function resetPassword(Request $request)
-    {
-        $user = User::where('id', $request->user_id)->first();
-
-        if ($this->userService->isVerificationCodeExpired($user)) {
-            return ApiResponse::sendResponse(403, "انتهت صلاحية رمز التحقق. يرجى طلب واحد جديدة.");
-        }
-
-        $isValidCode = $this->sms_service->checkOTPCodePassword($user->id, $request->code_verify);
-        if ($isValidCode) {
-            $request->validate(['password' => 'required|string|min:6|confirmed']);
-            $user->update(['password' => $request->password]);
-            return ApiResponse::sendResponse(200, 'Password reset successfully.');
-        } else {
-            return ApiResponse::sendResponse(200, 'Invalid verification code');
-        }
-    }
-
-    public function resendVerifyCode(Request $request)
-    {
-        $validator = Validator::make($request->all(), [
-            'user_id' => 'required',
-        ]);
-
-        if ($validator->fails()) {
-            return ApiResponse::sendResponse(422, "Validation Error", $validator->errors());
-        }
-
-        $user_id = $request->user_id;
-        $user = User::find($user_id);
-        if (!$user) {
-            return 'user id not found';
-        }
-
-        $data = ['user_id' => $user->id];
-        return $this->telephoneVerification->SendVerificationCode($user, $data);
-    }
-
-
+    $data = ['user_id' => $user->id];
+    return $this->telephoneVerification->SendVerificationCode($user, $data);
+  }
 }
