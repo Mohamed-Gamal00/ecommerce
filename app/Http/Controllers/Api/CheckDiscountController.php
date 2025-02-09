548a9e46142f67bd8eb9ae0ac97ce8b312f95977
Mohamed-Gamal00 <mohamedgamal1907a@gmail.com>
2025-02-09 12:18:17 +0200

    discount code refactoring

--- app/Http/Controllers/Api/CheckDiscountController.php
+++ app/Http/Controllers/Api/CheckDiscountController.php
@@ -4,93 +4,39 @@ namespace App\Http\Controllers\Api;
 
 use App\Helper\ApiResponse;
 use App\Http\Controllers\Controller;
-use App\Models\Cart;
-use App\Models\DiscountCode;
-use App\Models\UserDiscountCode;
-use Illuminate\Http\Request;
+use App\Http\Requests\Api\CheckDiscountCode;
+use App\Services\Discount\CheckDiscountService;
 
 class CheckDiscountController extends Controller
 {
-    public function __invoke(Request $request)
+    protected $checkDiscountCode;
+    public function __construct(CheckDiscountService $checkDiscount)
     {
-
-
-        // Validate the request
-        $request->validate([
-            'discount_code' => 'required|exists:discount_codes,code'
-        ]);
-
-        // Fetch the discount code and check its validity
-        $discountCode = DiscountCode::where('code', $request->discount_code)
-            ->where('status', 'active')
-            ->where('number_of_used', '>', 0)
-            ->first();
-        //        return $discountCode;
+        $this->checkDiscountCode = $checkDiscount;
+    }
+    public function __invoke(CheckDiscountCode $request)
+    {
+        $discountCode = $this->checkDiscountCode->fetchDiscountCode($request);
 
         if (!$discountCode) {
-            return response()->json([
-                'message' => 'لقد انتهت صلاحية رمز الخصم هذا أو لم يعد صالحًا.'
-            ], 400);
+            return ApiResponse::sendResponse(400, 'لقد انتهت صلاحية رمز الخصم هذا أو لم يعد صالحًا.');
         }
 
-        $userDiscountCode = UserDiscountCode::where([
-            'cookie_id' => Cart::getCookieIdApi(),
-            'discount_id' => $discountCode->id
-        ])->first();
-
-        if ($userDiscountCode) {
-            return response()->json([
-                'message' => 'لقد استخدمت هذا الكود بالفعل.'
-            ], 400);
-        } else {
-            UserDiscountCode::create([
-                'cookie_id' => Cart::getCookieIdApi(),
-                'discount_id' => $discountCode->id
-            ]);
+        $userUsedDiscountCode = $this->checkDiscountCode->checkIfUserUseDiscountCode($discountCode);
+        if ($userUsedDiscountCode) {
+            return $userUsedDiscountCode;
         }
+
         // Decrement the number of uses for this discount code
         $discountCode->decrement('number_of_used');
 
         // بشوف لو كود الخصم مش متحدد لمنتجات معينة هيبقي خصم علي كل المنتجات
         $isGlobalDiscount = $discountCode->products()->count() === 0;
 
-        // Apply discount to eligible cart items
         $user = $request->user();
 
-        $cartItems = Cart::with('product')->withoutGlobalScope('cookie_id')
-            ->where('user_id', $user->id)
-            ->where('status', 0)->get();
+        // Apply discount to cart items
+        return $this->checkDiscountCode->applyDiscountToCartItems($discountCode, $isGlobalDiscount, $user);
 
-        if ($cartItems->isEmpty()) {
-            return ApiResponse::sendResponse(200, 'لا يمكن استخدام كود الخصم والسلة فارغة');
-        }
-
-
-        foreach ($cartItems as $item) {
-            // موجودة في السلة $discountCode->products لو المنتجات اللي عليها خصم
-
-            if ($isGlobalDiscount || $discountCode->products->contains($item->product_id)) {
-                // Apply discount based on discount type نوع الخصم اذا كان نسبة او ثابت
-                if ($discountCode->discount_type == 'percentage') {
-                    $discountAmount = (($item->product->discount_price ?? $item->product->price) * $discountCode->price) / 100;
-                } else {
-                    $discountAmount = $discountCode->price;
-                }
-
-                // Set the discounted price
-                // نسبة الخصم علي المنتج الواحد
-
-                $originalPrice = $item->product->discount_price ?? $item->product->price;
-                $item->discounted_price = max(0, $originalPrice - $discountAmount);
-                $item->save();
-            }
-        }
-
-        return response()->json([
-            'message' => 'Discount code applied successfully.',
-            'discount_code' => $discountCode->code,
-            'discount_price' => $discountCode->price,
-            //            'cart' => $cartItems
-        ], 200);
     }
 }
